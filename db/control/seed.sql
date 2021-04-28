10c08dec2995a163278209a24cfb0292a3512452
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-04-28 16:41:48 -0500

    use explicit values in controll seed query

--- db/control/seed.sql
+++ db/control/seed.sql
@@ -3,20 +3,45 @@ DO $$
 
   DECLARE author_handle HANDLE;
   DECLARE post_handle HANDLE;
+  DECLARE topic_handle HANDLE;
 
 BEGIN
 
   FOR idx IN 1..1000 LOOP
 
-    INSERT INTO author(email, password) VALUES
-      (generate_string(), generate_string())
-    RETURNING handle INTO author_handle;
+      INSERT INTO author(handle, active, email, password, searchable, commenting)
+        VALUES (
+          DEFAULT,
+          DEFAULT,
+          generate_string(),
+          generate_string(),
+          DEFAULT,
+          DEFAULT
+        )
+      RETURNING handle INTO author_handle;
 
-    INSERT INTO post(author) VALUES
-      (author_handle)
-    RETURNING handle INTO post_handle;
+      INSERT INTO post(handle, author, active, header, content, style, searchable, commenting)
+        VALUES (
+          DEFAULT,
+          author_handle,
+          DEFAULT,
+          generate_string(),
+          generate_string(),
+          generate_string(),
+          DEFAULT,
+          DEFAULT
+        )
+      RETURNING handle INTO post_handle;
 
-    INSERT INTO topic(post) VALUES (post_handle);
+      INSERT INTO topic(handle, post, active, content, searchable)
+        VALUES (
+          DEFAULT,
+          post_handle,
+          DEFAULT,
+          generate_string(),
+          DEFAULT
+        )
+      RETURNING handle INTO topic_handle;
 
   END LOOP;
 
