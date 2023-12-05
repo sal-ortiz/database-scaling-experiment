cb9d1b9976205263768692ad8bd0abde4ac98d17
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2023-12-05 16:30:52 -0600

    use explicit values when seeding data

--- db/control/seed.sql
+++ db/control/seed.sql
@@ -11,60 +11,61 @@ BEGIN
 
   FOR idx IN 1..1000 LOOP
 
-      INSERT INTO usr(handle, username, active, password, searchable, commenting)
-        VALUES (
-          DEFAULT,
-          generate_string(),
-          DEFAULT,
-          generate_string(),
-          DEFAULT,
-          DEFAULT
-        )
-      RETURNING handle INTO usr_handle;
+    INSERT INTO usr(handle, username, active, password, searchable, commenting)
+      VALUES (
+        DEFAULT,
+        generate_string(),
+        generate_boolean(),
+        generate_string(),
+        generate_boolean(),
+        generate_boolean()
+      )
+    RETURNING handle INTO usr_handle;
 
-      INSERT INTO contact(handle, usr, email, phone, private, searchable)
-        VALUES (
-          DEFAULT,
-          usr_handle,
-          generate_string(),
-          DEFAULT,
-          DEFAULT,
-          DEFAULT
-        )
-      ;--RETURNING handle INTO contact_handle;
+    INSERT INTO contact(handle, usr, email, phone, private, searchable)
+      VALUES (
+        DEFAULT,
+        usr_handle,
+        generate_string(),
+        generate_string(),
+        generate_boolean(),
+        generate_boolean()
+      )
+    ;--RETURNING handle INTO contact_handle;
 
-      INSERT INTO author(handle, usr, searchable)
-        VALUES (
-          DEFAULT,
-          usr_handle,
-          DEFAULT
-        )
-      RETURNING handle INTO author_handle;
+    INSERT INTO author(handle, usr, searchable)
+      VALUES (
+        DEFAULT,
+        usr_handle,
+        generate_boolean()
+      )
+    RETURNING handle INTO author_handle;
 
-      INSERT INTO post(handle, author, active, header, content, style, searchable, commenting)
-        VALUES (
-          DEFAULT,
-          author_handle,
-          DEFAULT,
-          generate_string(),
-          generate_string(),
-          generate_string(),
-          DEFAULT,
-          DEFAULT
-        )
-      RETURNING handle INTO post_handle;
+    INSERT INTO post(handle, author, active, header, content, style, searchable, commenting)
+      VALUES (
+        DEFAULT,
+        author_handle,
+        generate_boolean(),
+        generate_string(),
+        generate_string(),
+        generate_string(),
+        generate_boolean(),
+        generate_boolean()
+      )
+    RETURNING handle INTO post_handle;
 
-      INSERT INTO topic(handle, post, active, content, searchable)
-        VALUES (
-          DEFAULT,
-          post_handle,
-          DEFAULT,
-          generate_string(),
-          DEFAULT
-        )
-      ;--RETURNING handle INTO topic_handle;
+    INSERT INTO topic(handle, post, active, content, searchable)
+      VALUES (
+        DEFAULT,
+        post_handle,
+        generate_boolean(),
+        generate_string(),
+        generate_boolean()
+      )
+    ;--RETURNING handle INTO topic_handle;
 
   END LOOP;
 
 END;
+
 $$;
--- db/variable/seed.sql
+++ db/variable/seed.sql
@@ -7,44 +7,45 @@ DO $$
   DECLARE post_handle HANDLE;
   DECLARE topic_handle HANDLE;
 
-BEGIN
+  BEGIN
 
-  FOR idx IN 1..1000 LOOP
+    FOR idx IN 1..1000 LOOP
 
-    INSERT INTO usr.handle(value) VALUES (DEFAULT) RETURNING value INTO usr_handle;
-    INSERT INTO usr.username(handle, value) VALUES (usr_handle, generate_string());
-    INSERT INTO usr.active(handle) VALUES (usr_handle);
-    INSERT INTO usr.password(handle, value) VALUES (usr_handle, generate_string());
-    INSERT INTO usr.searchable(handle) VALUES (usr_handle);
-    INSERT INTO usr.commenting(handle) VALUES (usr_handle);
+      INSERT INTO usr.handle(value) VALUES (DEFAULT) RETURNING value INTO usr_handle;
+      INSERT INTO usr.username(handle, value) VALUES (usr_handle, generate_string());
+      INSERT INTO usr.active(handle, value) VALUES (usr_handle, generate_boolean());
+      INSERT INTO usr.password(handle, value) VALUES (usr_handle, generate_string());
+      INSERT INTO usr.searchable(handle, value) VALUES (usr_handle, generate_boolean());
+      INSERT INTO usr.commenting(handle, value) VALUES (usr_handle, generate_boolean());
 
-    INSERT INTO contact.handle(value) VALUES (DEFAULT) RETURNING value INTO contact_handle;
-    INSERT INTO contact.usr(handle, value) VALUES (contact_handle, usr_handle);
-    INSERT INTO contact.email(handle, value) VALUES (contact_handle, generate_string());
-    INSERT INTO contact.phone(handle) VALUES (contact_handle);
-    INSERT INTO contact.private(handle) VALUES (contact_handle);
-    INSERT INTO contact.searchable(handle) VALUES (contact_handle);
+      INSERT INTO contact.handle(value) VALUES (DEFAULT) RETURNING value INTO contact_handle;
+      INSERT INTO contact.usr(handle, value) VALUES (contact_handle, usr_handle);
+      INSERT INTO contact.email(handle, value) VALUES (contact_handle, generate_string());
+      INSERT INTO contact.phone(handle, value) VALUES (contact_handle, generate_boolean());
+      INSERT INTO contact.private(handle, value) VALUES (contact_handle, generate_boolean());
+      INSERT INTO contact.searchable(handle, value) VALUES (contact_handle, generate_boolean());
 
-    INSERT INTO author.handle(value) VALUES (DEFAULT) RETURNING value INTO author_handle;
-    INSERT INTO author.usr(handle, value) VALUES (author_handle, usr_handle);
-    INSERT INTO author.searchable(handle) VALUES (author_handle);
+      INSERT INTO author.handle(value) VALUES (DEFAULT) RETURNING value INTO author_handle;
+      INSERT INTO author.usr(handle, value) VALUES (author_handle, usr_handle);
+      INSERT INTO author.searchable(handle, value) VALUES (author_handle, generate_boolean());
 
-    INSERT INTO post.handle(value) VALUES (DEFAULT) RETURNING value INTO post_handle;
-    INSERT INTO post.author(handle, value) VALUES (post_handle, author_handle);
-    INSERT INTO post.active(handle) VALUES (post_handle);
-    INSERT INTO post.header(handle) VALUES (post_handle);
-    INSERT INTO post.content(handle) VALUES (post_handle);
-    INSERT INTO post.style(handle) VALUES (post_handle);
-    INSERT INTO post.searchable(handle) VALUES (post_handle);
-    INSERT INTO post.commenting(handle) VALUES (post_handle);
+      INSERT INTO post.handle(value) VALUES (DEFAULT) RETURNING value INTO post_handle;
+      INSERT INTO post.author(handle, value) VALUES (post_handle, author_handle);
+      INSERT INTO post.active(handle, value) VALUES (post_handle, generate_boolean());
+      INSERT INTO post.header(handle, value) VALUES (post_handle, generate_string());
+      INSERT INTO post.content(handle, value) VALUES (post_handle, generate_string());
+      INSERT INTO post.style(handle, value) VALUES (post_handle, generate_string());
+      INSERT INTO post.searchable(handle, value) VALUES (post_handle, generate_boolean());
+      INSERT INTO post.commenting(handle, value) VALUES (post_handle, generate_boolean());
 
-    INSERT INTO topic.handle(value) VALUES (DEFAULT) RETURNING value INTO topic_handle;
-    INSERT INTO topic.post(handle, value) VALUES (topic_handle, post_handle);
-    INSERT INTO topic.active(handle) VALUES (topic_handle);
-    INSERT INTO topic.content(handle) VALUES (topic_handle);
-    INSERT INTO topic.searchable(handle) VALUES (topic_handle);
+      INSERT INTO topic.handle(value) VALUES (DEFAULT) RETURNING value INTO topic_handle;
+      INSERT INTO topic.post(handle, value) VALUES (topic_handle, post_handle);
+      INSERT INTO topic.active(handle, value) VALUES (topic_handle, generate_boolean());
+      INSERT INTO topic.content(handle, value) VALUES (topic_handle, generate_string());
+      INSERT INTO topic.searchable(handle, value) VALUES (topic_handle, generate_boolean());
 
-  END LOOP;
+    END LOOP;
 
-END;
+  END;
+
 $$;
